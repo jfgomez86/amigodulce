533fc6c6c1e191ce8fdf9a6bf4bffabf36ad566b
Jose Felix Gomez <jfgomez86@gmail.com>
2011-09-06 15:57:04 -0500

    Bugfix for unnormalized names

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
-require 'scrabbler'
+require './scrabbler'
 require 'sinatra'
 
 get "/:token/:name" do
   encrypted = File.read("result.txt").chomp
   data = Scrabbler.decrypt(encrypted)
-  name = params[:name].split(/_/).map(&:capitalize).join(" ")
-  if data.key?(params[:token]) && data[params[:token]].key?(name)
-    data[params[:token ]][name]
+  name = params[:name].gsub(/_/, " ").upcase
+  if data.key?(params[:token]) && data[params[:token]].keys.map(&:upcase).include?(name)
+    data[params[:token ]].inspect
   else
     "Nadie me quiere...todos me odian... voy a comerme un gusaniitoo :("
   end
